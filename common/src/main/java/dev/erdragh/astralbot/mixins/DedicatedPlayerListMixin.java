56db912dcc5bcc695febedbfbfabe1f0fa95324e
Erdragh <mail@erdragh.dev>
2023-12-01 18:57:16 +0100

    Fix whitelist check not being executed

---
+++ common/src/main/java/dev/erdragh/astralbot/mixins/DedicatedPlayerListMixin.java
@@ -0,0 +1,16 @@
+package dev.erdragh.astralbot.mixins;
+
+import com.mojang.authlib.GameProfile;
+import dev.erdragh.astralbot.handlers.WhitelistHandler;
+import net.minecraft.server.dedicated.DedicatedPlayerList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(DedicatedPlayerList.class)
+public class DedicatedPlayerListMixin {  @Inject(method = "isWhiteListed", at = @At("RETURN"), cancellable = true)
+    void astralbot$isWhiteListed(GameProfile profile, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(Boolean.TRUE.equals(cir.getReturnValue()) || WhitelistHandler.INSTANCE.checkWhitelist(profile.getId()) != null);
+    }
+}
