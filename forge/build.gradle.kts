378c8da0fa75f81b4eef3052ade9715010369f39
Erdragh <mail@erdragh.dev>
2023-12-03 12:21:02 +0100

    Add comments to forge build script

--- forge/build.gradle.kts
+++ forge/build.gradle.kts
@@ -7,6 +7,9 @@ loom {
         mixinConfig("astralbot-common.mixins.json")
         mixinConfig("astralbot.mixins.json")
     }
+    // This sets up data generation. At the time of writing this
+    // Comment, this is useless, as there are no resources to be
+    // generated. I want to keep it in as a reference tho.
     runs {
         create("data") {
             data()
@@ -36,12 +39,13 @@ dependencies {
     val kotlinForgeVersion: String by project
 
     forge(group = "net.minecraftforge", name = "forge", version = "$minecraftVersion-$forgeVersion")
-    // Adds KFF as dependency and Kotlin libs (use the variant matching your mod loader)
-    // FORGE
+    // Adds KFF as dependency and Kotlin libs
     implementation("thedarkcolour:kotlinforforge:$kotlinForgeVersion")
 
     val jdaVersion: String by project
 
+    // This *should* theoretically fix the Forge development environment not having
+    // access to certain classes, but I haven't gotten it to work just yet.
     forgeRuntimeLibrary("net.dv8tion:JDA:$jdaVersion") {
         exclude(module = "opus-java")
         exclude(group = "org.jetbrains.kotlin")
