ee2c547504017125f1d9514b05918c67d8112ceb
Erdragh <mail@erdragh.dev>
2023-12-03 13:48:17 +0100

    Add comments to the AstralBotConfig

--- common/src/main/kotlin/dev/erdragh/astralbot/config/AstralBotConfig.kt
+++ common/src/main/kotlin/dev/erdragh/astralbot/config/AstralBotConfig.kt
@@ -2,10 +2,27 @@ package dev.erdragh.astralbot.config
 
 import net.minecraftforge.common.ForgeConfigSpec
 
+/**
+ * Config for the AstralBot mod. This uses Forge's config system
+ * to reduce dependencies. This can be used on Fabric by using
+ * the Forge Config API port.
+ * @author Erdragh
+ */
 object AstralBotConfig {
     val SPEC: ForgeConfigSpec
 
+    /**
+     * Whether the default whitelisting process is respected or ignored.
+     * Setting this to `true` will *force* every user who wants to join
+     * the server to link their account, even Operators.
+     */
     val REQUIRE_LINK_FOR_WHITELIST: ForgeConfigSpec.BooleanValue
+
+    /**
+     * The link to the Discord servers where users can run the /link command.
+     * Gets used when formatting the message shown to users who try to log in
+     * without being whitelisted.
+     */
     val DISCORD_LINK: ForgeConfigSpec.ConfigValue<String>
 
     init {
