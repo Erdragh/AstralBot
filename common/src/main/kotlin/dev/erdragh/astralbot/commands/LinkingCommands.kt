7ec8f35b8f6b9be888f5edc7ec12b697be7437cc
Erdragh <mail@erdragh.dev>
2023-12-03 14:07:04 +0100

    Add comments to LinkingCommands

--- common/src/main/kotlin/dev/erdragh/astralbot/commands/LinkingCommands.kt
+++ common/src/main/kotlin/dev/erdragh/astralbot/commands/LinkingCommands.kt
@@ -1,6 +1,7 @@
 package dev.erdragh.astralbot.commands
 
 import com.mojang.authlib.GameProfile
+import dev.erdragh.astralbot.LOGGER
 import dev.erdragh.astralbot.handlers.WhitelistHandler
 import dev.erdragh.astralbot.minecraftHandler
 import net.dv8tion.jda.api.entities.Member
@@ -10,27 +11,42 @@ import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.Commands
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData
 
+/**
+ * This command links a Discord user to a Minecraft account.
+ *
+ * It has the following options:
+ * - `code`: the link code the user got from the screen trying to log into the server
+ *
+ * @author Erdragh
+ */
 object LinkCommand : HandledSlashCommand {
+    // Specifying option names as constants to prevent typos
     private const val OPTION_CODE = "code"
 
     override val command: SlashCommandData =
-        Commands.slash("link", "Links your Minecraft Account with your Discord Account")
+        Commands.slash("link", "Links your Minecraft account with your Discord account")
             .addOption(OptionType.NUMBER, OPTION_CODE, "your personal link code", true)
 
     override fun handle(event: SlashCommandInteractionEvent) {
+        // DB Interactions could take a while, so the reply needs to get deferred
         event.deferReply(true).queue()
+
+        // Get the Minecraft user ID from the link code
         val linkCode = event.getOption(OPTION_CODE)?.asString
         val linkCodeInt = linkCode?.toDoubleOrNull()?.toInt()
         val minecraftID = if (linkCodeInt != null) WhitelistHandler.getPlayerFromCode(linkCodeInt) else null
 
+        // Notify the user that the given link code couldn't be associated with a Minecraft account
         if (minecraftID == null) {
-            event.hook.setEphemeral(true).sendMessage("Couldn't find minecraft account to link").queue()
+            event.hook.setEphemeral(true).sendMessage("Couldn't find Minecraft account to link").queue()
             return
         }
 
+        // Gets the user profile of the given Minecraft user ID to get access to the name
         val minecraftUser = minecraftHandler?.byUUID(minecraftID)
 
         try {
+            // Depending on the whitelisting status of the given data send the relevant response
             if (WhitelistHandler.checkWhitelist(minecraftID) != null) {
                 event.hook.setEphemeral(true)
                     .sendMessageFormat("Minecraft username %s already linked", minecraftUser?.name)
@@ -43,41 +59,69 @@ object LinkCommand : HandledSlashCommand {
                     .sendMessageFormat("Linked %s to Minecraft username %s", event.member, minecraftUser?.name).queue()
             }
         } catch (e: Exception) {
+            // Just in case a DB interaction failed the user still needs to get a response.
             event.hook.setEphemeral(true).sendMessageFormat("Failed to link: %s", e.localizedMessage).queue()
+            LOGGER.error("Failed to link", e)
         }
 
     }
 }
 
+/**
+ * This command unlinks the issuing Discord user
+ *
+ * @author Erdragh
+ */
 object UnlinkCommand : HandledSlashCommand {
     override val command: SlashCommandData =
-        Commands.slash("unlink", "Unlinks your Minecraft Account with your Discord Account")
+        Commands.slash("unlink", "Unlinks your Minecraft account with your Discord account")
 
     override fun handle(event: SlashCommandInteractionEvent) {
+        // DB Interactions could take a while, so the reply needs to get deferred
         event.deferReply(true).queue()
+
         WhitelistHandler.unWhitelist(event.user)
 
         event.hook.setEphemeral(true).sendMessageFormat("Unlinked %s", event.user).queue()
     }
 }
 
+/**
+ * This command can check the link status of Discord Users and Minecraft accounts
+ *
+ * It has the following options:
+ * - `mc`: a Minecraft username for which a possibly linked Discord User will be reported
+ * - `dc`: a Discord user for which a possibly linked Minecraft account will be reported
+ *
+ * @author Erdragh
+ */
 object LinkCheckCommand : HandledSlashCommand, AutocompleteCommand {
+    // Specifying option names as constants to prevent typos
     private const val OPTION_MC = "mc"
     private const val OPTION_DC = "dc"
 
-    override val command = Commands.slash("linkcheck", "Checks link status of a specified Minecraft or Discord Account")
+    override val command = Commands.slash("linkcheck", "Checks link status of a specified Minecraft or Discord account")
         .addOption(OptionType.STRING, OPTION_MC, "Minecraft Username", false, true)
         .addOption(OptionType.MENTIONABLE, OPTION_DC, "Discord User", false)
 
+    /**
+     * Handles the case the command issuer gave a Minecraft name
+     * @param event the issued event for the command. Used to respond
+     * @param minecraftName the Minecraft username the issuer provided
+     */
     private fun handleMinecraftToDiscord(event: SlashCommandInteractionEvent, minecraftName: String) {
+        // Since this message gets used in separate places the template is declared earlier
         val notFound = "Minecraft username %s is not linked to any Discord User"
 
+        // Gets the associated [GameProfile] for the name to get the ID
         val minecraftProfile = minecraftHandler?.byName(minecraftName)
 
+        // gets the discord ID
         val discordID = if (minecraftProfile != null) WhitelistHandler.checkWhitelist(minecraftProfile.id) else null
         if (discordID != null) {
             val guild = event.guild
             if (guild != null) {
+                // TODO: Replace with more performant solution
                 var found = false
                 guild.loadMembers {
                     if (it.idLong == discordID) {
@@ -109,6 +153,11 @@ object LinkCheckCommand : HandledSlashCommand, AutocompleteCommand {
         }
     }
 
+    /**
+     * Handles the case the command issuer gave a Discord user
+     * @param event the issued event for the command. Used to respond
+     * @param discordUser the Discord user the issuer provided
+     */
     private fun handleDiscordToMinecraft(event: SlashCommandInteractionEvent, discordUser: Member) {
         val minecraftID = WhitelistHandler.checkWhitelist(discordUser.idLong)
         if (minecraftID != null) {
@@ -124,6 +173,8 @@ object LinkCheckCommand : HandledSlashCommand, AutocompleteCommand {
     }
 
     override fun handle(event: SlashCommandInteractionEvent) {
+        // Since DB interactions and sending request to Discord may take a while
+        // the response gets deferred
         event.deferReply(true).queue()
 
         val minecraftName = event.getOption(OPTION_MC)?.asString
@@ -149,6 +200,7 @@ object LinkCheckCommand : HandledSlashCommand, AutocompleteCommand {
 
     override fun autocomplete(event: CommandAutoCompleteInteractionEvent) {
         if (event.focusedOption.name == OPTION_MC) {
+            // TODO: Maybe cache this for servers with a lot of members?
             val minecraftUsers = minecraftHandler?.getOnlinePlayers()?.map(GameProfile::getName)
             event.replyChoiceStrings(minecraftUsers?.filter { it.startsWith(event.focusedOption.value) } ?: listOf())
                 .queue()
