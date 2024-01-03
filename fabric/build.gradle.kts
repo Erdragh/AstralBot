977ec61fc8b465a9c96177e2165c499e5649ac8f
Erdragh <mail@erdragh.dev>
2024-01-03 09:48:48 +0100

    Update dependencies

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -19,15 +19,12 @@ dependencies {
     val minecraftVersion: String by project
     val fabricLoaderVersion: String by project
     val fabricApiVersion: String by project
-    val modMenuVersion: String by project
     val fabricKotlinVersion: String by project
     val forgeConfigAPIVersion: String by project
 
     modImplementation(group = "net.fabricmc", name = "fabric-loader", version = fabricLoaderVersion)
     modApi(group = "net.fabricmc.fabric-api", name = "fabric-api", version = "$fabricApiVersion+$minecraftVersion")
     modImplementation("net.fabricmc:fabric-language-kotlin:${fabricKotlinVersion}")
-
-    modApi(group = "com.terraformersmc", name = "modmenu", version = modMenuVersion)
 
     modApi("fuzs.forgeconfigapiport:forgeconfigapiport-fabric:$forgeConfigAPIVersion")
 }
