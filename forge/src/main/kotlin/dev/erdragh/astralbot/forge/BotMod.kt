c0bc5a85eb7ea2d5a7fefd072c0c621733432f55
Erdragh <mail@erdragh.dev>
2024-04-16 15:42:48 +0200

    Register link command on forge

--- forge/src/main/kotlin/dev/erdragh/astralbot/forge/BotMod.kt
+++ forge/src/main/kotlin/dev/erdragh/astralbot/forge/BotMod.kt
@@ -29,6 +29,7 @@ object BotMod {
         FORGE_BUS.addListener(::onServerStop)
         FORGE_BUS.addListener(::onChatMessage)
         FORGE_BUS.addListener(::onSystemMessage)
+        FORGE_BUS.addListener(::onCommandRegistration)
 
         FORGE_BUS.addListener(::onPlayerJoin)
         FORGE_BUS.addListener(::onPlayerLeave)
